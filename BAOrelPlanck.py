43def4c31517b99a1ff71fe814edd40631d3c6ba
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2019-12-20 22:37:05 -0500

    relative BAO

--- BAOrelPlanck.py
+++ BAOrelPlanck.py
@@ -13,7 +13,28 @@ rcParams['font.family'] = 'serif'
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=14)
 
-
+def plotBAOrelBOSSfid_dv(labels):
+	#reads in dv/rs measurements, plots them relative to the expectation in the BOSS fiducial cosmology
+	dcosfid = distance(.31,.69,.676,obhh=0.022)
+	rsfidcamb = 147.77 #r_s(z_drag) from camb and fiducial cosmology
+	rsfidEH =  dcosfid.rs
+	meas = open('BAOcomp.txt').readlines()
+	#columns in meas are
+	#year, ref, label, zeff, dvrs, sigdv, dmrs, sigdm, hrs, sigh, omfid, hfid, omb2fid, rsEH
+	for i in range(4,len(meas)):
+		ln = meas[i].split(',')
+		lab = ln[2]
+		if np.isin(lab,labels):
+			z = float(ln[3])
+			dv = float(ln[4])
+			sdv = float(ln[5])
+			EH = int(ln[-1].strip('\n'))
+			if EH:
+				dvrsfid = dcosfid.dV(z)/rsfidEH
+			else:
+				dvrsfid = dcosfid.dV(z)/rsfidcamb
+			plt.errorbar(z,dv/dvrsfid,sdv/dvrsfid,fmt='o')
+	plt.show()	
 
 def BAOrelPlanckCurrent(wo='current',xmax=2.5,Lya=True,BOSS=False,BOSSDR12=True,MGS=True,wz=True,sdss=False,df6=True,QSODR14=True,LRGDR14=True,des=False,desy1=True,eboss=False,desi=False):
 	import matplotlib.pyplot as plt
